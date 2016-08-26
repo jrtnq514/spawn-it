aeb1d60ac9c71fea488f82c60931d66130f3eb0f
Nathan Francy <nathanfrancy@gmail.com>
2016-08-25 23:02:22 -0500

    Fixing typo in female first name test.

--- test/modules/string.js
+++ test/modules/string.js
@@ -66,7 +66,7 @@ describe('STRING', function () {
             });
 
             it('should return name from original female list', function (done) {
-                let result = string.names.firstName('male');
+                let result = string.names.firstName('female');
                 let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
 
                 indexOfFemaleName.should.not.equal(-1);
