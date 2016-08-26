3b386cc0568de20040d39ef92114a85044e92bd8
Nathan Francy <nathanfrancy@gmail.com>
2016-08-25 23:14:53 -0500

    Adding myself to travis emails. Altering code to cover 'else' block in firstName fn.

--- test/modules/string.js
+++ test/modules/string.js
@@ -12,66 +12,98 @@ var stringConfig = require('./../../lib/modules/config/string');
 describe('STRING', function () {
 
     describe('names', function () {
-        describe('firstName - male', function () {
-            it('should return a male first name as a string', function (done) {
-                let result = string.names.firstName('male');
-                let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
 
-                result.should.be.a('string');
-                indexOfMaleFirstName.should.not.equal(null);
-                indexOfMaleFirstName.should.not.equal(undefined);
-                indexOfMaleFirstName.should.not.equal(-1);
-                done();
+        describe('firstName', function() {
+
+            describe('male', function () {
+
+                it('should return a male first name as a string', function (done) {
+                    let result = string.names.firstName('male');
+                    let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
+
+                    result.should.be.a('string');
+                    indexOfMaleFirstName.should.not.equal(null);
+                    indexOfMaleFirstName.should.not.equal(undefined);
+                    indexOfMaleFirstName.should.not.equal(-1);
+                    done();
+                });
+
+                it('should not return null or undefined male name', function (done) {
+                    let result = string.names.firstName('male');
+                    let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
+
+                    result.should.be.a('string');
+                    indexOfMaleFirstName.should.not.equal(null);
+                    indexOfMaleFirstName.should.not.equal(undefined);
+                    done();
+                });
+
+                it('should return name from original male list', function (done) {
+                    let result = string.names.firstName('male');
+                    let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
+
+                    indexOfMaleFirstName.should.not.equal(-1);
+                    done();
+                });
+
             });
 
-            it('should not return null or undefined male name', function (done) {
-                let result = string.names.firstName('male');
-                let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
+            describe('female', function() {
 
-                result.should.be.a('string');
-                indexOfMaleFirstName.should.not.equal(null);
-                indexOfMaleFirstName.should.not.equal(undefined);
-                done();
+                it('should return a female first name as a string', function (done) {
+                    let result = string.names.firstName('female');
+                    let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
+
+                    result.should.be.a('string');
+                    indexOfFemaleName.should.not.equal(null);
+                    indexOfFemaleName.should.not.equal(undefined);
+                    indexOfFemaleName.should.not.equal(-1);
+                    done();
+                });
+
+                it('should not return null or undefined female name', function (done) {
+                    let result = string.names.firstName('female');
+                    let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
+
+                    result.should.be.a('string');
+                    indexOfFemaleName.should.not.equal(null);
+                    indexOfFemaleName.should.not.equal(undefined);
+                    done();
+                });
+
+                it('should return name from original female list', function (done) {
+                    let result = string.names.firstName('female');
+                    let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
+
+                    indexOfFemaleName.should.not.equal(-1);
+                    done();
+                });
+
             });
 
-            it('should return name from original male list', function (done) {
-                let result = string.names.firstName('male');
-                let indexOfMaleFirstName = stringConfig.male.indexOf(result.toUpperCase());
+            describe('no specific gender', function() {
 
-                indexOfMaleFirstName.should.not.equal(-1);
-                done();
+                it('should return a name even if passed in gender is invalid', function (done) {
+                    // Passing invalid gender should still give a name
+                    let result = string.names.firstName('idk');
+
+                    result.should.not.equal(null);
+                    result.should.not.equal(undefined);
+                    done();
+                });
+
+                it('should return a name in the combined array of names', function(done) {
+                    let names = stringConfig.male.concat(stringConfig.female);
+                    let result = string.names.firstName('idk');
+                    let indexName = names.indexOf(result.toUpperCase());
+
+                    indexName.should.not.equal(-1);
+                    done();
+                });
+
             });
+
         });
 
-        describe('firstName - female', function() {
-            it('should return a female first name as a string', function (done) {
-                let result = string.names.firstName('female');
-                let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
-
-                result.should.be.a('string');
-                indexOfFemaleName.should.not.equal(null);
-                indexOfFemaleName.should.not.equal(undefined);
-                indexOfFemaleName.should.not.equal(-1);
-                done();
-            });
-
-            it('should not return null or undefined female name', function (done) {
-                let result = string.names.firstName('female');
-                let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
-
-                result.should.be.a('string');
-                indexOfFemaleName.should.not.equal(null);
-                indexOfFemaleName.should.not.equal(undefined);
-                done();
-            });
-
-            it('should return name from original female list', function (done) {
-                let result = string.names.firstName('female');
-                let indexOfFemaleName = stringConfig.female.indexOf(result.toUpperCase());
-
-                indexOfFemaleName.should.not.equal(-1);
-                done();
-            });
-        });
     });
 });
