59aaad12832edfa8089401498ef3a4d943dad5c0
Josh Taylor <burggamer.jt@gmail.com>
2016-08-26 18:02:11 -0500

    Added decade to date with tests.

--- test/modules/date.js
+++ test/modules/date.js
@@ -7,6 +7,7 @@ var should = require('chai').should();
 // module to test
 var date = require('./../../lib/modules/date');
 var dateConfig = require('./../../lib/modules/config/date');
+var moment = require('moment');
 
 describe('DATE', function () {
 
@@ -117,6 +118,70 @@ describe('DATE', function () {
 
                 result.should.be.an('array');
                 result.should.equal(expectedNames);
+                done();
+            });
+        });
+    });
+
+    describe('year', function () {
+        describe('full', function () {
+            it('should return a year between 1900 and the current year', function (done) {
+                let currentYear = moment().format('YYYY');
+                let result = date.year.full();
+
+                result.should.be.a('number');
+                result.should.be.within(1900, currentYear);
+                done();
+            });
+
+            it('should return a year between the provided min and max', function (done) {
+                let min = 1800;
+                let max = 2017;
+                let result = date.year.full();
+
+                result.should.be.a('number');
+                result.should.be.within(min, max);
+                done();
+            });
+        });
+
+        describe('full', function () {
+            it('should return the last two digits of a year (00 - 99)', function (done) {
+                let result = date.year.short();
+                
+                result.should.be.a('string');
+                result.should.be.within(0, 99);
+                done();
+            });
+        });
+    });
+    
+    describe('decade', function () {
+        describe('full', function () {
+            it('should return the full year value of a decade', function(done) {
+                let result = date.decade.full();
+
+                result.should.be.a('string');
+                done();
+            });
+        });
+
+        describe('short', function () {
+            it('should return the short value of a decade', function(done) {
+                let result = date.decade.short();
+
+                result.should.be.a('string');
+                done();
+            });
+        });
+
+        describe('name', function () {
+            it('should return the spelled out name of a decade', function(done) {
+                let expectedNames = dateConfig.decadeNames;
+                let result = date.decade.name();
+                
+                result.should.be.a('string');
+                expectedNames.should.include(result);
                 done();
             });
         });
