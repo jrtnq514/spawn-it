256bd4ac13147a9c8e114906cc67a5cd4f08be6b
Nathan Francy <nathanfrancy@gmail.com>
2016-08-28 22:37:48 -0500

    Adding location resource and implementations for city, state and planet. Adding a handful of unit tests for these.

--- test/modules/location.js
+++ test/modules/location.js
@@ -1,3 +1,57 @@
 /**
  * Created by JT on 8/26/16.
  */
+
+ var chai = require('chai'),
+     should = chai.should(),
+     expect = chai.expect,
+     assert = chai.assert;
+
+var location = require('./../../lib/modules/location');
+
+describe('LOCATION', function() {
+
+    describe('city', function() {
+
+        it('should return a string', function(done) {
+            var city = location.city('MO');
+            expect(city).to.be.a('string');
+            expect(city).to.not.equal(null);
+            expect(city).to.not.equal(undefined);
+            done();
+        });
+
+    });
+
+    describe('state', function() {
+
+        it('should return an object', function(done) {
+            var state = location.state();
+            expect(state).to.be.a('object');
+            expect(state).to.not.equal(null);
+            expect(state).to.not.equal(undefined);
+            done();
+        });
+
+        it('should return a state and a code', function(done) {
+            var state = location.state();
+            expect(state.code).to.not.equal(undefined);
+            expect(state.state).to.not.equal(undefined);
+            done();
+        });
+
+    });
+
+    describe('planet', function() {
+
+        it('should return a string', function(done) {
+            var planet = location.planet();
+            expect(planet).to.be.a('string');
+            expect(planet).to.not.equal(null);
+            expect(planet).to.not.equal(undefined);
+            done();
+        });
+
+    });
+
+});
