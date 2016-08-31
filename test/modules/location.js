53307df9bdce7f379594a1cd3c62c5841d9427bf
Nathan Francy <nathanfrancy@gmail.com>
2016-08-30 22:04:31 -0500

    Refactoring planet data into own file and adding config files for countries and continents. Updating unit tests and implementations for countries, continents and planets.

--- test/modules/location.js
+++ test/modules/location.js
@@ -9,6 +9,11 @@
 
 var location = require('./../../lib/modules/location');
 
+var locationConfig = require('./../../lib/modules/config/location');
+var planets = locationConfig.planets;
+var countries = locationConfig.countries;
+var continents = locationConfig.continents;
+
 describe('LOCATION', function() {
 
     describe('city', function() {
@@ -61,6 +66,48 @@ describe('LOCATION', function() {
             expect(planet).to.be.a('string');
             expect(planet).to.not.equal(null);
             expect(planet).to.not.equal(undefined);
+            done();
+        });
+
+        it('returns valid planet', function(done) {
+            var planet = location.planet();
+            expect(planets.indexOf(planet)).to.not.equal(-1);
+            done();
+        });
+
+    });
+
+    describe('country', function() {
+
+        it('should return a string', function(done) {
+            var country = location.country();
+            expect(country).to.be.a('string');
+            expect(country).to.not.equal(null);
+            expect(country).to.not.equal(undefined);
+            done();
+        });
+
+        it('returns valid country', function(done) {
+            var country = location.country();
+            expect(countries.indexOf(country)).to.not.equal(-1);
+            done();
+        });
+
+    });
+
+    describe('continent', function() {
+
+        it('should return a string', function(done) {
+            var continent = location.continent();
+            expect(continent).to.be.a('string');
+            expect(continent).to.not.equal(null);
+            expect(continent).to.not.equal(undefined);
+            done();
+        });
+
+        it('returns valid continent', function(done) {
+            var continent = location.continent();
+            expect(continents.indexOf(continent)).to.not.equal(-1);
             done();
         });
 
